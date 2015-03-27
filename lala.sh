bbf00ab4699bd910bbfd25e42fd6d2d717cb536a
Steve Masterman <steve@vermonster.com>
2015-03-27 15:03:49 -0400

    lala

---
+++ lala.sh
@@ -0,0 +1,24 @@
+say -v 'Agnes' lalalalala &
+say -v 'Albert' lalalalala &
+say -v 'Alex' lalalalala &
+say -v 'Bad News' lalalalala &
+say -v 'Bahh' lalalalala &
+say -v 'Bells' lalalalala &
+say -v 'Boing' lalalalala &
+say -v 'Bruce' lalalalala &
+say -v 'Bubbles' lalalalala &
+say -v 'Cellos' lalalalala &
+say -v 'Deranged' lalalalala &
+say -v 'Fred' lalalalala &
+say -v 'Good News' lalalalala &
+say -v 'Hysterical' lalalalala &
+say -v 'Junior' lalalalala &
+say -v 'Kathy' lalalalala &
+say -v 'Pipe Organ' lalalalala &
+say -v 'Princess' lalalalala &
+say -v 'Ralph' lalalalala &
+say -v 'Trinoids' lalalalala &
+say -v 'Vicki' lalalalala &
+say -v 'Victoria' lalalalala &
+say -v 'Whisper' lalalalala &
+say -v 'Zarvox' lalalalala &
